9bc5e691eb5361aae7397170ae3fb089b09cb767
Bodor István <istvanbodor19@gmail.com>
2022-09-29 08:49:55 +0200

    2 elemű lista elkészítve

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -1,4 +1,21 @@
 package hu.petrik.bejegyzesprojekt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    static List<Bejegyzes> bejegyzesek = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+        bejegyzesek.add(new Bejegyzes("Bodor István","Ez egy bejegyzés"));
+        bejegyzesek.add(new Bejegyzes("Bodor István","Ez a második bejegyzés"));
+
+
+    }
+
+
+
 }
