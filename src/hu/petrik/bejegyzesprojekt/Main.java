9b063d812ed06ed057cf31399abfe85901bcf8d4
Bodor István <istvanbodor19@gmail.com>
2022-09-29 09:11:22 +0200

    Elemek bekérése felhasználótól

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -2,6 +2,7 @@ package hu.petrik.bejegyzesprojekt;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -12,10 +13,24 @@ public class Main {
 
         bejegyzesek.add(new Bejegyzes("Bodor István","Ez egy bejegyzés"));
         bejegyzesek.add(new Bejegyzes("Bodor István","Ez a második bejegyzés"));
+        hozzafuzobekeres();
 
 
     }
 
+    private static void hozzafuzobekeres()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adja meg hány bejegyzést szeretne felvenni: ");
+        int db = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i <db ; i++) {
+            System.out.printf("Adja meg a(z) %d bejegyzés szerzőjét:",i+1);
+            String szerzo = sc.nextLine();
+            System.out.printf("Adja meg a(z) %d bejegyzés tartalmát:", i+1);
+            String tartalom = sc.nextLine();
+            bejegyzesek.add(new Bejegyzes(szerzo, tartalom));
+        }
 
-
+    }
 }
